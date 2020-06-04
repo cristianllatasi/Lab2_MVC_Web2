4922adda8a49d5fb3412d8627874dae21c26a870
Cristian Llatasi <cristianllatasi@gmail.com>
2020-06-04 13:13:23 -0500

    Clase Estudiante

---
+++ Lab2_MVC_LLATASI/Models/ClsEstudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2_MVC_LLATASI.Models
+{
+    public class ClsEstudiante
+    {
+        public string apellido { get; set; }
+        public string nombre { get; set; }
+        public int codigoestudiante { get; set; }
+
+        public int carrera { get; set; }
+    }
+}
